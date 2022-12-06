446bdefb806aa194c0c6b3eb3b874baba636f4b9
Tabby Toney Douglass <tabbytoney@gmail.com>
2022-12-05 21:24:58 -0600

    Setting up express, creating apis, using dummy data

---
+++ backend/data/data.js
@@ -0,0 +1,34 @@
+const chats = [
+  {
+    isGroupChat: false,
+    users: [
+      {
+        name: 'John Doe',
+        email: 'john@example.com',
+      },
+      {
+        name: 'Burt Lastname',
+        email: 'Burt@example.com',
+      },
+    ],
+    _id: '234ro23ufn234hn',
+    chatName: 'John Doe',
+  },
+  {
+    isGroupChat: false,
+    users: [
+      {
+        name: 'Jane Doe',
+        email: 'jane@example.com',
+      },
+      {
+        name: 'Janet AnotherName',
+        email: 'janet@example.com',
+      },
+    ],
+    _id: '234ro23ufn2fese2q3w3',
+    chatName: 'Jane Doe',
+  },
+];
+
+module.exports = { chats };
