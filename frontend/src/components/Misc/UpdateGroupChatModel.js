0233457c263cde1a3c300fc7aaa70d6b76a139f2
Tabby Toney Douglass <tabbytoney@gmail.com>
2022-12-11 18:35:12 -0600

    edit  group chat ui

---
+++ frontend/src/components/Misc/UpdateGroupChatModel.js
@@ -0,0 +1,293 @@
+import { MdOutlineSettings } from 'react-icons/md';
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Button,
+  useDisclosure,
+  FormControl,
+  Input,
+  useToast,
+  Box,
+  IconButton,
+  Spinner,
+} from '@chakra-ui/react';
+import axios from 'axios';
+import { useState } from 'react';
+import { ChatState } from '../../Context/ChatProvider';
+import UserBadgeItem from '../UserAvatar/UserBadgeItem';
+import UserListItem from '../UserAvatar/UserListItem';
+
+const UpdateGroupChatModal = ({ fetchMessages, fetchAgain, setFetchAgain }) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [groupChatName, setGroupChatName] = useState();
+  const [search, setSearch] = useState('');
+  const [searchResult, setSearchResult] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [renameloading, setRenameLoading] = useState(false);
+  const toast = useToast();
+
+  const { selectedChat, setSelectedChat, user } = ChatState();
+
+  const handleSearch = async (query) => {
+    setSearch(query);
+    if (!query) {
+      return;
+    }
+
+    try {
+      setLoading(true);
+      const config = {
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+        },
+      };
+      const { data } = await axios.get(`/api/user?search=${search}`, config);
+      console.log(data);
+      setLoading(false);
+      setSearchResult(data);
+    } catch (error) {
+      toast({
+        title: 'Error Occured!',
+        description: 'Failed to Load the Search Results',
+        status: 'error',
+        duration: 5000,
+        isClosable: true,
+        position: 'bottom-left',
+      });
+      setLoading(false);
+    }
+  };
+
+  const handleRename = async () => {
+    if (!groupChatName) return;
+
+    try {
+      setRenameLoading(true);
+      const config = {
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+        },
+      };
+      // how to use the api to update group chat name
+      const { data } = await axios.put(
+        `/api/chat/rename`,
+        {
+          chatId: selectedChat._id,
+          chatName: groupChatName,
+        },
+        config
+      );
+
+      console.log(data._id);
+      // setSelectedChat("");
+      setSelectedChat(data);
+      setFetchAgain(!fetchAgain);
+      setRenameLoading(false);
+    } catch (error) {
+      toast({
+        title: 'Error occured!',
+        description: error.response.data.message,
+        status: 'error',
+        duration: 5000,
+        isClosable: true,
+        position: 'bottom',
+      });
+      setRenameLoading(false);
+    }
+    setGroupChatName('');
+  };
+
+  const handleAddUser = async (user1) => {
+    if (selectedChat.users.find((u) => u._id === user1._id)) {
+      toast({
+        title: 'User Already in group!',
+        status: 'error',
+        duration: 5000,
+        isClosable: true,
+        position: 'bottom',
+      });
+      return;
+    }
+
+    if (selectedChat.groupAdmin._id !== user._id) {
+      toast({
+        title: 'Only admins can add someone!',
+        status: 'error',
+        duration: 5000,
+        isClosable: true,
+        position: 'bottom',
+      });
+      return;
+    }
+
+    try {
+      setLoading(true);
+      const config = {
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+        },
+      };
+      const { data } = await axios.put(
+        `/api/chat/groupadd`,
+        {
+          chatId: selectedChat._id,
+          userId: user1._id,
+        },
+        config
+      );
+
+      setSelectedChat(data);
+      setFetchAgain(!fetchAgain);
+      setLoading(false);
+    } catch (error) {
+      toast({
+        title: 'Error Occured!',
+        description: error.response.data.message,
+        status: 'error',
+        duration: 5000,
+        isClosable: true,
+        position: 'bottom',
+      });
+      setLoading(false);
+    }
+    setGroupChatName('');
+  };
+
+  const handleRemove = async (user1) => {
+    if (selectedChat.groupAdmin._id !== user._id && user1._id !== user._id) {
+      toast({
+        title: 'Only admins can remove someone!',
+        status: 'error',
+        duration: 5000,
+        isClosable: true,
+        position: 'bottom',
+      });
+      return;
+    }
+
+    try {
+      setLoading(true);
+      const config = {
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+        },
+      };
+      const { data } = await axios.put(
+        `/api/chat/groupremove`,
+        {
+          chatId: selectedChat._id,
+          userId: user1._id,
+        },
+        config
+      );
+      // if the user removes themselves, we'll set selected chat to empty - bc they've left the group
+      user1._id === user._id ? setSelectedChat() : setSelectedChat(data);
+      setFetchAgain(!fetchAgain);
+      fetchMessages();
+      setLoading(false);
+    } catch (error) {
+      toast({
+        title: 'Error Occured!',
+        description: error.response.data.message,
+        status: 'error',
+        duration: 5000,
+        isClosable: true,
+        position: 'bottom',
+      });
+      setLoading(false);
+    }
+    setGroupChatName('');
+  };
+
+  return (
+    <>
+      <IconButton
+        display={{ base: 'flex' }}
+        icon={<MdOutlineSettings size='25px' />}
+        onClick={onOpen}
+        bgColor='beige'
+      />
+
+      <Modal onClose={onClose} isOpen={isOpen} isCentered>
+        <ModalOverlay />
+        <ModalContent bgColor='beige'>
+          <ModalHeader
+            fontSize='35px'
+            fontFamily='Work sans'
+            display='flex'
+            justifyContent='center'
+            color='orange.900'
+          >
+            {selectedChat.chatName}
+          </ModalHeader>
+
+          <ModalCloseButton />
+          <ModalBody display='flex' flexDir='column' alignItems='center'>
+            <Box w='100%' display='flex' flexWrap='wrap' pb={3}>
+              {selectedChat.users.map((u) => (
+                <UserBadgeItem
+                  key={u._id}
+                  user={u}
+                  admin={selectedChat.groupAdmin}
+                  handleFunction={() => handleRemove(u)}
+                />
+              ))}
+            </Box>
+            <FormControl display='flex'>
+              <Input
+                placeholder='Chat Name'
+                mb={3}
+                value={groupChatName}
+                onChange={(e) => setGroupChatName(e.target.value)}
+                borderColor='orange.300'
+                focusBorderColor='orange.500'
+              />
+              <Button
+                variant='solid'
+                colorScheme='orange'
+                ml={1}
+                isLoading={renameloading}
+                onClick={handleRename}
+              >
+                Update
+              </Button>
+            </FormControl>
+            <FormControl>
+              <Input
+                placeholder='Add User to group'
+                mb={1}
+                borderColor='orange.300'
+                focusBorderColor='orange.500'
+                onChange={(e) => handleSearch(e.target.value)}
+              />
+            </FormControl>
+
+            {loading ? (
+              <Spinner size='lg' duration='2ms' />
+            ) : (
+              searchResult?.map((user) => (
+                <UserListItem
+                  key={user._id}
+                  user={user}
+                  handleFunction={() => handleAddUser(user)}
+                />
+              ))
+            )}
+          </ModalBody>
+          <ModalFooter>
+            <Button onClick={() => handleRemove(user)} colorScheme='red'>
+              Leave Group
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
+
+export default UpdateGroupChatModal;
---
+++ frontend/src/components/SingleChat.js
@@ -0,0 +1,96 @@
+// import './styles.css';
+import {
+  IconButton,
+  Spinner,
+  useToast,
+  Box,
+  Text,
+  Input,
+  FormControl,
+} from '@chakra-ui/react';
+import { MdArrowBack } from 'react-icons/md';
+import { getSender, getSenderFull } from '../config/ChatLogics';
+// import { useEffect, useState } from 'react';
+// import axios from 'axios';
+
+import ProfileModal from './Misc/ProfileModal';
+// import ScrollableChat from './ScrollableChat';
+// import Lottie from 'react-lottie';
+// import animationData from '../animations/typing.json';
+
+// import io from 'socket.io-client';
+import UpdateGroupChatModal from './Misc/UpdateGroupChatModel';
+import { ChatState } from '../Context/ChatProvider';
+// const ENDPOINT = 'http://localhost:3000'; // "https://talk-a-tive.herokuapp.com"; -> After deployment
+// var socket, selectedChatCompare;
+
+const SingleChat = ({ fetchAgain, setFetchAgain }) => {
+  const { user, selectedChat, setSelectedChat } = ChatState();
+
+  return (
+    <>
+      {selectedChat ? (
+        <>
+          <Text
+            fontSize={{ base: '28px', md: '30px' }}
+            pb={3}
+            px={2}
+            w='100%'
+            fontFamily='Work sans'
+            display='flex'
+            justifyContent={{ base: 'space-between' }}
+            alignItems='center'
+            color='orange.900'
+          >
+            <IconButton
+              display={{ base: 'flex', md: 'none' }}
+              icon={<MdArrowBack size='20px' />}
+              bgColor='beige'
+              onClick={() => setSelectedChat('')}
+            />
+            {!selectedChat.isGroupChat ? (
+              <>
+                {getSender(user, selectedChat.users)}
+                <ProfileModal user={getSenderFull(user, selectedChat.users)} />
+              </>
+            ) : (
+              <>
+                {selectedChat.chatName.toUpperCase()}
+                <UpdateGroupChatModal
+                  fetchAgain={fetchAgain}
+                  setFetchAgain={setFetchAgain}
+                />
+              </>
+            )}
+          </Text>
+          <Box
+            display='flex'
+            flexDir='column'
+            justifyContent='flex-end'
+            p={3}
+            bg='orange.100'
+            w='100%'
+            h='100%'
+            borderRadius='lg'
+            overflowY='hidden'
+          >
+            Messages here
+          </Box>
+        </>
+      ) : (
+        <Box
+          display='flex'
+          alignItems='center'
+          justifyContent='center'
+          h='100%'
+        >
+          <Text fontSize='3xl' pb={3} fontFamily='Work sans' color='orange.900'>
+            Click on a user to start chatting
+          </Text>
+        </Box>
+      )}
+    </>
+  );
+};
+
+export default SingleChat;
